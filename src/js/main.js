8f8836a1a66b2805fc7a696de1f05e9cc6e6fabb
ArrrtSam <artemsamilenko19999@gmail.com>
2019-02-02 00:09:31 +0200

    Added portfolio container

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,13 @@ $("#hamburger-btn").click(function() {
   $("#menu").toggleClass("menu--active");
   $(this).toggleClass("is-active");
 });
-$(function() {
-  $(".toggles button").click(function() {
-    var get_id = this.id;
-    console.log('get_id: ' + get_id);
-    var get_current = ".post." + get_id;
-    console.log('get_current: ' + get_current);
 
-    $(".post")
-      .not($(get_current))
-      .hide(500);
-    $(get_current).show(500);
-  });
-  $("#showall").click(function() {
-    $(".post").show(500);
-  });
+// init Isotope
+var $grid = $(".posts").isotope({
+  // options
+});
+// filter items on button click
+$(".filter-button-group").on("click", "button", function() {
+  var filterValue = $(this).attr("data-filter");
+  $grid.isotope({ filter: filterValue });
 });
